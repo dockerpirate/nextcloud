ea9abe5cb75255b370bfa1807822bf876c94636f
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-11-11 23:26:08 +0100

    Create ci.sh

---
+++ ci.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TAG_2="${TRAVIS_TAG:-latest}"
+
+if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+  docker buildx build \
+    --progress plain \
+    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
+    .
+  exit $?
+fi
+echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
+
+docker buildx build \
+     --build-arg ALPINE_M \
+     --build-arg ALPINE_P \
+     --build-arg UNBOUND_M \
+     --build-arg UNBOUND_P \
+     --build-arg LDNS \
+     --progress plain \
+    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
+    -t $DOCKER_REPO:$TAG_2 \
+    --push .
