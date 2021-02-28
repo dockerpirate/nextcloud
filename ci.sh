7e117cabbe004ac85130bf89a57c5dcb39272e28
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2021-02-28 23:39:41 +0100

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,13 +3,13 @@
 TAG_1="$NEXT_M"."$NEXT_P"
 TAG_2="${TRAVIS_TAG:-latest}"
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
-  docker buildx build \
-    --progress plain \
-    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
-    .
-  exit $?
-fi
+#if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
+#  docker buildx build \
+#    --progress plain \
+#    --platform=linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64,linux/386 \
+#    .
+#  exit $?
+#fi
 echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
 
 docker buildx build \
